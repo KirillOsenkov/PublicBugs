d44c933908b5d74295ca6116aeb074e70c69c516
Kirill Osenkov <github@osenkov.com>
2023-12-13 15:10:24 -0800

    Repro

--- CscAnal/analyzer/Class1.cs
+++ CscAnal/analyzer/Class1.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
@@ -9,16 +11,32 @@ public class Generator : ISourceGenerator
 {
     public void Execute(GeneratorExecutionContext context)
     {
-        var assembly = typeof(SchemaExtensions).Assembly;
-        SchemaExtensions.Validate(null, null, null);
-        File.AppendAllLines(@"analyzer.log", new string[] { assembly.Location });
-
+        //try
+        {
+            M();
+        }
+        //catch { }
+        G();
         context.AddSource("example.cs", SourceText.From(text: """
             class J { }
             """, encoding: Encoding.UTF8));
     }
 
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    void M()
+    {
+        Newtonsoft.Json.Schema.SchemaExtensions.Validate(null, null, null);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    void G()
+    {
+        var assembly = typeof(SchemaExtensions).Assembly;
+        File.AppendAllLines(@"analyzer.log", new string[] { assembly.Location });
+    }
+
     public void Initialize(GeneratorInitializationContext context)
     {
+        Debugger.Launch();
     }
 }
