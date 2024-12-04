61c2982c82cc97ed2d13deeb3315872ffd973700
Kirill Osenkov <github@osenkov.com>
2024-12-03 17:07:30 -0800

    Add a repro

---
+++ SystemRuntimeLoader/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var poolManager = typeof(MySql.Data.MySqlClient.MySqlConnection).Assembly.GetType("MySql.Data.MySqlClient.MySqlPoolManager");
+        var instance = Activator.CreateInstance(poolManager);
+    }
+}
